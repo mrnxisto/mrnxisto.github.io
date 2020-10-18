9d6c06ef62cdbf1e7e3efa753a270559443d3cb8
Marina Xisto Almeida <contatoxisto@gmail.com>
2020-10-17 23:33:21 -0300

    Nova versão da página

--- README.md
+++ README.md
@@ -8,7 +8,6 @@
 
 ## Conteúdo
 
-- Skills
 - Portfólio
 - Sobre mim
 - Contato
---
+++ js/script.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  hamburger.addEventListener("click", () => {
+    // Toogle nav
+    nav.classList.toggle("nav-active");
+
+    // Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    // Hamburger menu animation
+    hamburger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
